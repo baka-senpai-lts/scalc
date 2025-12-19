582eecd685710b16a4191a05921d597dbc0137e5
baka <baka.senpai.lts@gmail.com>
2025-12-19 13:47:00 +0200

    ref: fix formatting

--- repl.c
+++ repl.c
@@ -6,19 +6,19 @@
 
 int sc_getline(const char *prompt, char *buffer, int size) {
   if (prompt != NULL) {
-	fputs(prompt, stdout);
+    fputs(prompt, stdout);
   }
 
   if (fgets(buffer, size, stdin) == NULL) {
-	return REPL_EMPTY;
+    return REPL_EMPTY;
   }
 
-  if(buffer[0] == '\0') {
-	return REPL_EMPTY;
+  if (buffer[0] == '\0') {
+    return REPL_EMPTY;
   }
 
-  if(buffer[sc_strlen(buffer)-1] != '\n') {
-	return REPL_TOO_LONG;
+  if (buffer[sc_strlen(buffer) - 1] != '\n') {
+    return REPL_TOO_LONG;
   }
 
   return REPL_OK;
